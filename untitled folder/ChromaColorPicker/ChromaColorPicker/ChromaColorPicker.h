6b8e838e701027ef164a5283957fed24b2121180
Yazdan Vakili <ymazdy@gmail.com>
2018-06-05 14:11:29 +0430

    added Carthage support

---
+++ untitled folder/ChromaColorPicker/ChromaColorPicker/ChromaColorPicker.h
@@ -0,0 +1,19 @@
+//
+//  ChromaColorPicker.h
+//  ChromaColorPicker
+//
+//  Created by Yazdan Vakili on 6/5/18.
+//  Copyright © 2018 mandaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChromaColorPicker.
+FOUNDATION_EXPORT double ChromaColorPickerVersionNumber;
+
+//! Project version string for ChromaColorPicker.
+FOUNDATION_EXPORT const unsigned char ChromaColorPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChromaColorPicker/PublicHeader.h>
+
+
